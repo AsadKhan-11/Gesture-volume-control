afa186b4399d1714f1548df422a01f025a5569ca
Asad Khan <mrasad10khan@gmail.com>
2024-06-22 14:12:06 +0500

    Detecting thumb and index finger

--- volumeControl.py
+++ volumeControl.py
@@ -1,19 +1,44 @@
 import cv2
 import numpy as num
 import time
+import HandTrackingModule as htm
+import mediapipe as mp
 
 
 hCam , wCam  = 480 , 640
- 
 
 
-cap = cv2.VideoCapture(0)  
+
+cap = cv2.VideoCapture(0)
 cap.set(3,wCam)
 cap.set(4,hCam)
 pTime = 0
 
+
+detector = htm.handDetector(detection_con=0.7)
+
+
+
 while True:
     success, img = cap.read()
+
+    img = detector.find_hands(img)
+    lmList= detector.find_position(img , draw=False)
+    
+    if len(lmList) > 8:
+        try:
+            x1, y1 = lmList[4][1], lmList[4][2]
+            x2, y2 = lmList[8][1], lmList[8][2]
+            print("Thumb Tip:", (x1, y1))  
+            print("Index Finger Tip:", (x2, y2))  
+
+            cv2.circle(img,(x1,y1), 12, (255,0,0), cv2.FILLED)
+            cv2.circle(img,(x2,y2), 12, (255,0,0), cv2.FILLED)
+        except IndexError as e:
+            print("IndexError occurred:", e)
+    # else:
+    #    print("Not enough landmarks detected")  
+
     if success:
 
         cTime = time.time()
